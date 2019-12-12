70950582e9cfa30a48dbbde984d3f316c4d7a507
Fredrik H <fredrik@pool.fi>
2019-12-12 13:17:04 +0200

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+        <p>Please login to view dashboard</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
